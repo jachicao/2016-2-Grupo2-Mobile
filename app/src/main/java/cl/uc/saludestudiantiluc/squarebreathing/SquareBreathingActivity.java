102c50c367ed1bab2a072e182cabd7c09950d007
Lukas Zorich <lukas.zorich@gmail.com>
2016-09-20 21:53:46 -0300

    Fix square breathing bug.
    The circles where tinkering on Android 4.2

--- app/src/main/java/cl/uc/saludestudiantiluc/squarebreathing/SquareBreathingActivity.java
+++ app/src/main/java/cl/uc/saludestudiantiluc/squarebreathing/SquareBreathingActivity.java
@@ -2,15 +2,14 @@ package cl.uc.saludestudiantiluc.squarebreathing;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import cl.uc.saludestudiantiluc.R;
 
 public class SquareBreathingActivity extends AppCompatActivity {
 
   private static final String TAG = SquareBreathingActivity.class.getSimpleName();
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
