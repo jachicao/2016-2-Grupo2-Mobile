6c5b3babeab2acecbfddabfa10adf3f0f4194d1c
jachicao <jachicao@uc.cl>
2016-09-27 20:19:54 -0300

    Add immerse sequences.

--- app/src/main/java/cl/uc/saludestudiantiluc/sequences/ImagesActivity.java
+++ app/src/main/java/cl/uc/saludestudiantiluc/sequences/ImagesActivity.java
@@ -1,11 +1,13 @@
 package cl.uc.saludestudiantiluc.sequences;
 
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 import cl.uc.saludestudiantiluc.R;
 
@@ -21,9 +23,16 @@ public class ImagesActivity extends FragmentActivity {
     if (extras != null) {
       Sequence sequence = extras.getParcelable(getString(R.string.sequences_parcelable_name));
       if (sequence != null) {
-        mPager = (ViewPager) findViewById(R.id.sequencesViewPager);
+        mPager = (ViewPager) findViewById(R.id.sequences_view_pager);
         mPagerAdapter = new ImagesFragmentPagerAdapter(getSupportFragmentManager(), sequence);
         mPager.setAdapter(mPagerAdapter);
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if (Build.VERSION.SDK_INT >= 19) {
+          uiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
       }
     }
   }
