d1dc1f08f1a6092f7acfa9920b49cf7e07039c0e
Lukas Zorich <lukas.zorich@gmail.com>
2016-11-17 01:28:48 -0300

    Remove unused imports

--- app/src/main/java/cl/uc/saludestudiantiluc/sequences/SequencesListAdapter.java
+++ app/src/main/java/cl/uc/saludestudiantiluc/sequences/SequencesListAdapter.java
@@ -1,19 +1,10 @@
 package cl.uc.saludestudiantiluc.sequences;
 
 import android.view.View;
-import android.widget.Button;
 
-import java.io.File;
-import java.util.ArrayList;
-
-import cl.uc.saludestudiantiluc.R;
 import cl.uc.saludestudiantiluc.common.MediaListAdapter;
 import cl.uc.saludestudiantiluc.common.MediaListFragment;
-import cl.uc.saludestudiantiluc.common.models.Media;
 import cl.uc.saludestudiantiluc.sequences.models.Sequence;
-import cl.uc.saludestudiantiluc.services.download.DownloadService;
-import cl.uc.saludestudiantiluc.services.download.FilesListener;
-import cl.uc.saludestudiantiluc.services.download.FilesRequest;
 
 /**
  * Created by jchicao on 15-09-16.
