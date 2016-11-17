d4eb24afcc40f9f4e611a5271073da4cb428e234
jachicao <jachicao@uc.cl>
2016-11-17 12:23:16 -0300

    Remove vector assets on notification

--- app/src/main/java/cl/uc/saludestudiantiluc/sos/SosService.java
+++ app/src/main/java/cl/uc/saludestudiantiluc/sos/SosService.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
@@ -26,7 +27,7 @@ public class SosService extends Service {
   private NotificationCompat.Builder mNotifyBuilder;
   private NotificationManager mNotificationManager;
   private BroadcastReceiver mReceiver;
-  private boolean mNotificated;
+  private boolean mNotified;
 
 
   public SosService() {
@@ -59,7 +60,7 @@ public class SosService extends Service {
 
     SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
     boolean notifyPref = sharedPref.getBoolean("notification", true);
-    if (notifyPref){
+    if (notifyPref) {
       mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -74,17 +75,15 @@ public class SosService extends Service {
 
               Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
               context.sendBroadcast(it);
-            } else {
-              setNotificationView(true);
             }
           }
         }
       };
-      mNotificated = true;
+      mNotified = true;
       showNotification();
     } else {
-      if(mNotificated) {
-        mNotificated = false;
+      if(mNotified) {
+        mNotified = false;
         undoNotification();
       }
 
@@ -94,20 +93,27 @@ public class SosService extends Service {
   private void undoNotification() {
     try {
       mNotificationManager.cancel(1);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
 
     }
   }
 
 
   private void showNotification() {
-    mNotifyBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable
-        .ic_favorite_black_24dp);
+    mNotifyBuilder = new NotificationCompat.Builder(this);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      mNotifyBuilder.setSmallIcon(R.drawable.ic_favorite_black_24dp);
+    } else {
+      mNotifyBuilder.setSmallIcon(R.drawable.ic_favorite_white_24dp);
+    }
 
     mNotifyBuilder.setOngoing(true);
     mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-    RemoteViews collapsedView = setNotificationView(true);
+    RemoteViews collapsedView = new RemoteViews(getPackageName(), R.layout.sos_notification_collapsed);
+    collapsedView.setImageViewResource(R.id.sos_notification_button, R.drawable.ic_relaxing);
+    collapsedView.setImageViewResource(R.id.sos_notification_icon, R.drawable
+        .ic_favorite_black_24dp);
     mNotifyBuilder.setCustomContentView(collapsedView);
 
     //SOS event
@@ -118,17 +124,7 @@ public class SosService extends Service {
     PendingIntent pendingIntent = PendingIntent.getBroadcast(this, MESSAGE_START_STOP, new Intent
         (SOS), PendingIntent.FLAG_UPDATE_CURRENT);
 
-    collapsedView.setOnClickPendingIntent(R.id.sos_notification_play_and_pause, pendingIntent);
+    collapsedView.setOnClickPendingIntent(R.id.sos_notification_button, pendingIntent);
     mNotificationManager.notify(1, mNotifyBuilder.build());
-  }
-
-  private RemoteViews setNotificationView(boolean isCollapsed) {
-    RemoteViews contentView = new RemoteViews(getPackageName(), isCollapsed ? R.layout
-        .sos_notification_collapsed : R
-        .layout.sos_notification_collapsed);
-    contentView.setImageViewResource(R.id.sos_notification_play_and_pause, R.drawable.ic_relaxing );
-    contentView.setImageViewResource(R.id.sos_notification_icon, R.drawable
-        .ic_favorite_black_24dp);
-    return contentView;
   }
 }
