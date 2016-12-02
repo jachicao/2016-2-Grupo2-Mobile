ea2d99189324097f2089a08d94033596ee46f961
jachicao <jachicao@uc.cl>
2016-12-02 18:11:51 -0300

    Fix rut

--- app/src/main/java/cl/uc/saludestudiantiluc/auth/AuthActivity.java
+++ app/src/main/java/cl/uc/saludestudiantiluc/auth/AuthActivity.java
@@ -187,28 +187,32 @@ public class AuthActivity extends BaseActivity {
     return password.length() > 4;
   }
 
+  public static String cleanRut(String rut) {
+    return rut.replace(".", "").replace("-", "");
+  }
+
   public boolean isRutValid(String rut) {
+    return true;
+    /*
     try {
-      rut = rut.toUpperCase();
-      rut = rut.replace(".", "");
-      rut = rut.replace("-", "");
-      int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
-
-      char dv = rut.charAt(rut.length() - 1);
-
-      int m = 0;
-      int s = 1;
-      for (; rutAux != 0; rutAux /= 10) {
-        s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+      int suma = 0;
+      String dvR = "";
+      String dvT = "";
+      int[] serie = {2, 3, 4, 5, 6, 7};
+      dvR = rut.substring(rut.length() - 1);
+      for (int i = rut.length() - 2; i >= 0; i--) {
+        suma += Integer.valueOf(rut.substring(i, i + 1)) * serie[(rut.length() - 2 - i) % 6];
       }
-      if (dv == (char) (s != 0 ? s + 47 : 75)) {
-        return true;
+      dvT = String.valueOf(11 - suma % 11);
+      if (dvT.compareToIgnoreCase("10") == 0) {
+        dvT = "K";
       }
 
+      return dvT.compareToIgnoreCase(dvR) == 0;
     } catch (java.lang.NumberFormatException e) {
       return false;
     }
-    return false;
+    */
   }
 
   @Override
